8592424a9801bd5a343ed8e749e0b895d3fd6b4e
MrMikey695 <mikeedubas@hotmail.com>
2022-10-25 15:12:03 +0300

    functionality about 80% sure its good. Now need to clean

--- srcs/ft_deletion_shift.c
+++ srcs/ft_deletion_shift.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_deletion_shift.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: mbarutel <mbarutel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 14:37:39 by mrantil           #+#    #+#             */
-/*   Updated: 2022/10/20 16:25:31 by mrantil          ###   ########.fr       */
+/*   Updated: 2022/10/25 15:05:32 by mbarutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,18 @@
 
 void	ft_deletion_shift(char *input, t_term *term, int mode)
 {
-	int	cur_cpy;
+	int	indx_cpy;
 
-	cur_cpy = term->bytes;
 	if (mode == BCK)
+		term->indx--;
+	indx_cpy = term->indx;
+	input[indx_cpy] = '\0';
+	while (&input[indx_cpy] < &input[term->bytes])
 	{
-		cur_cpy--;
-		term->bytes = cur_cpy;
+		input[indx_cpy] = input[indx_cpy] ^ input[indx_cpy + 1];
+		input[indx_cpy + 1] = input[indx_cpy] ^ input[indx_cpy + 1];
+		input[indx_cpy] = input[indx_cpy] ^ input[indx_cpy + 1];
+		indx_cpy++;
 	}
-	input[cur_cpy] = '\0';
-	while (&input[cur_cpy] < &input[term->c_col])
-	{
-		input[cur_cpy] = input[cur_cpy] ^ input[cur_cpy + 1];
-		input[cur_cpy + 1] = input[cur_cpy] ^ input[cur_cpy + 1];
-		input[cur_cpy] = input[cur_cpy] ^ input[cur_cpy + 1];
-		cur_cpy++;
-	}
-	term->c_col--;
+	term->bytes--;
 }
