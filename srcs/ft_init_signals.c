a9402ce9e100ee0366696f2ca403f85e6d4bfea7
MrMikey695 <mikeedubas@hotmail.com>
2022-11-04 14:24:17 +0200

    rows in regards history and srolling to your initial input

--- srcs/ft_init_signals.c
+++ srcs/ft_init_signals.c
@@ -6,7 +6,7 @@
 /*   By: mbarutel <mbarutel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 18:17:16 by mrantil           #+#    #+#             */
-/*   Updated: 2022/11/03 17:33:58 by mbarutel         ###   ########.fr       */
+/*   Updated: 2022/11/04 14:18:08 by mbarutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ extern t_term	*g_term;
 static void	sig_handler(int num)
 {
 	if (num == SIGWINCH)
+	{
 		ft_window_size(g_term);
+		setup_nl_addr(g_term, char *input)
+	}
 	/* if (num == SIGINT)
 	{
 		ft_disable_raw_mode();
