0f9725ba28aadce04cbb44ba98c45581383f16f8
MrMikey695 <mikeedubas@hotmail.com>
2022-11-04 13:46:09 +0200

    qoute handling improved for change of input via history trigger

--- srcs/ft_arrow_input.c
+++ srcs/ft_arrow_input.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_arrow_input.c                                     :+:      :+:    :+:   */
+/*   ft_arrow_input.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbarutel <mbarutel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 16:27:59 by mrantil           #+#    #+#             */
-/*   Updated: 2022/11/03 16:04:44 by mbarutel         ###   ########.fr       */
+/*   Updated: 2022/11/04 11:23:17 by mbarutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,75 +46,6 @@ static void	ft_left(char *input, t_term *term)
 				- term->nl_addr[--term->c_row]);
 	}
 	ft_setcursor(term->c_col, term->c_row);
-}
-
-static void	setup_nl_addr(t_term *term, char *input)
-{
-	size_t	i;
-	size_t	len;
-	size_t	prompt_len;
-
-	i = 0;
-	len = 0;
-	term->total_row = 0;
-	ft_memdel((void **)&term->nl_addr);
-	add_nl_last_row(term, input, i);
-	while (input[++i])
-	{
-		len++;
-		prompt_len = 0;
-		if (!term->total_row)
-			prompt_len = term->prompt_len;
-		else if (is_prompt_line(term, term->total_row))
-			prompt_len = term->m_prompt_len;
-		if ((len + prompt_len) / (term->ws_col) || input[i] == '\n')
-		{
-			add_nl_last_row(term, input, i + 1);
-			term->total_row++;
-			len = 0;
-		}
-	}
-}
-
-static void	ft_history_trigger(t_term *term, char *input, int his)
-{
-	char *history;
-
-	history = NULL;
-	term->c_col = 0;
-	term->c_row = 0;
-	ft_setcursor(term->c_col, term->c_row);
-	ft_run_capability("cd");
-	ft_putstr(PROMPT);
-	history = (char *)vec_get(&term->v_history, term->v_history.len - his);
-	if (history)
-	{
-		term->bytes = ft_strlen(history);
-		term->input_cpy = ft_strdup(input);
-		ft_memset((void *)input, '\0', BUFF_SIZE);
-		ft_memcpy((void *)input, (void *)history, term->bytes);
-		setup_nl_addr(term, input);
-	}
-	else
-	{
-		term->bytes = ft_strlen(term->input_cpy);
-		ft_memset((void *)input, '\0', BUFF_SIZE);
-		ft_memcpy((void *)input, (void *)term->input_cpy, term->bytes);	
-		ft_strdel(&term->input_cpy);
-		setup_nl_addr(term, input);
-	}
-	ft_print_trail(term, input);
-	term->index = term->bytes;
-	term->c_row = term->total_row;
-	if (is_prompt_line(term, term->c_row))
-	{
-		if (!term->c_row)
-			term->c_col = term->prompt_len;
-		else
-			term->c_col = term->m_prompt_len;
-	}
-	term->c_col += &input[term->bytes] - term->nl_addr[term->c_row];
-	ft_setcursor(term->c_col, term->total_row);
 }
 
 void	ft_arrow_input(t_term *term, char *input)
---
+++ srcs/ft_history_trigger.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_history_trigger.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbarutel <mbarutel@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/04 10:59:10 by mbarutel          #+#    #+#             */
+/*   Updated: 2022/11/04 13:42:25 by mbarutel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "keyboard.h"
+
+static size_t	get_prompt_len(t_term *term, size_t row)
+{
+	size_t	prompt_len;
+
+	prompt_len = 0;
+	if (!row)
+		prompt_len = term->prompt_len;
+	else if (is_prompt_line(term, row))
+		prompt_len = term->m_prompt_len;
+	return (prompt_len);
+}
+
+static void	setup_nl_addr(t_term *term, char *input)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	len = 0;
+	term->total_row = 0;
+	ft_memdel((void **)&term->nl_addr);
+	add_nl_last_row(term, input, i);
+	while (input[++i])
+	{
+		len++;
+		if (((len + get_prompt_len(term, term->total_row)) + 1) / (term->ws_col) || input[i] == '\n')
+		{
+			add_nl_last_row(term, input, i + 1);
+			term->total_row++;
+			len = -1;
+		}
+		if (input[i] == D_QUO || input[i] == S_QUO)
+			quote_handling(term, input[i]);
+	}
+}
+
+static void	ft_history_trigger_start(t_term *term)
+{
+	ft_run_capability("vi");
+	term->quote = 0;
+	term->q_qty = 0;
+	term->c_col = 0;
+	term->c_row = 0;
+	ft_setcursor(term->c_col, term->c_row);
+	ft_run_capability("cd");
+	ft_putstr(PROMPT);
+}
+
+static void	ft_history_trigger_end(t_term *term, char *input)
+{
+	term->index = term->bytes;
+	term->c_row = term->total_row;
+	if (is_prompt_line(term, term->c_row))
+	{
+		if (!term->c_row)
+			term->c_col = term->prompt_len;
+		else
+			term->c_col = term->m_prompt_len;
+	}
+	term->c_col += &input[term->bytes] - term->nl_addr[term->c_row];
+	ft_setcursor(term->c_col, term->total_row);
+	ft_run_capability("ve");
+}
+
+void	ft_history_trigger(t_term *term, char *input, int his)
+{
+	char	*history;
+
+	ft_history_trigger_start(term);
+	history = (char *)vec_get(&term->v_history, term->v_history.len - his);
+	if (history)
+	{
+		term->bytes = ft_strlen(history);
+		term->input_cpy = ft_strdup(input);
+		ft_memset((void *)input, '\0', BUFF_SIZE);
+		ft_memcpy((void *)input, (void *)history, term->bytes);
+	}
+	else
+	{
+		term->bytes = ft_strlen(term->input_cpy);
+		ft_memset((void *)input, '\0', BUFF_SIZE);
+		ft_memcpy((void *)input, (void *)term->input_cpy, term->bytes);
+		ft_strdel(&term->input_cpy);
+	}
+	setup_nl_addr(term, input);
+	ft_print_trail(term, input);
+	ft_history_trigger_end(term, input);
+}
