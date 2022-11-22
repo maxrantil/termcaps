4eb642d92b3633d78f8a046551d837f6e547a117
Max <rantil@pm.me>
2022-11-22 11:54:02 +0200

    SIGINT

--- srcs/ft_init_signals.c
+++ srcs/ft_init_signals.c
@@ -6,7 +6,7 @@
 /*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 18:17:16 by mrantil           #+#    #+#             */
-/*   Updated: 2022/11/11 10:22:10 by mrantil          ###   ########.fr       */
+/*   Updated: 2022/11/22 11:53:13 by mrantil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,15 @@ static void	sig_handler(int num)
 {
 	if (num == SIGWINCH)
 		ft_window_size(g_t);
+	if (num == SIGINT)
+	{
+		ft_putchar('\n');
+		ft_restart_cycle(g_t);
+	}
 }
 
 void	ft_init_signals(void)
 {
 	signal(SIGWINCH, sig_handler);
+	signal(SIGINT, sig_handler);
 }
