7066f1c5e416830e8fa8746ccd5c9e9b0e9afc1b
Max <rantil@pm.me>
2022-11-28 13:51:44 +0200

    remove newline with ctrl_c

--- srcs/ft_init_signals.c
+++ srcs/ft_init_signals.c
@@ -6,7 +6,7 @@
 /*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 18:17:16 by mrantil           #+#    #+#             */
-/*   Updated: 2022/11/22 11:53:13 by mrantil          ###   ########.fr       */
+/*   Updated: 2022/11/28 13:51:08 by mrantil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,7 @@ static void	sig_handler(int num)
 	if (num == SIGWINCH)
 		ft_window_size(g_t);
 	if (num == SIGINT)
-	{
-		ft_putchar('\n');
 		ft_restart_cycle(g_t);
-	}
 }
 
 void	ft_init_signals(void)
