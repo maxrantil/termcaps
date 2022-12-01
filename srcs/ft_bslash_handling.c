4c7ea8a19a80069947fcde4c86843b70d68d5095
MrMikey695 <mikeedubas@hotmail.com>
2022-12-01 13:03:39 +0200

    improved backslash handling when a backslash is deleted + added variable to contain result of ctrl_d() rather than running the function twice

--- srcs/ft_bslash_handling.c
+++ srcs/ft_bslash_handling.c
@@ -3,40 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_bslash_handling.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: mbarutel <mbarutel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 16:28:08 by mbarutel          #+#    #+#             */
-/*   Updated: 2022/11/29 17:10:33 by mrantil          ###   ########.fr       */
+/*   Updated: 2022/12/01 13:02:05 by mbarutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "keyboard.h"
 
-/*
- * If the character is a backslash, and the index is at the end of the input,
- * and we're not in a
- * heredoc, then we check to see if the number of backslashes is odd or even.
- * If it's odd, then we set
- * the bslash flag to 1, otherwise we set it to 0
- *
- * @param t the structure that holds all the information about the current
- * state of the terminal.
+/**
+ * It checks if the last character is a backslash, and if it is, it checks if
+ * it's escaped or not.
+ * 
+ * @param t the structure that holds all the information about the terminal
  */
 void	ft_bslash_handling(t_term *t)
 {
 	ssize_t	i;
 
-	i = t->index - 1;
-	if (t->ch == '\\' && t->index == t->bytes && !t->heredoc)
+	i = t->bytes - 1;
+	if (t->c_row == t->total_row)
 	{
-		while (i && t->inp[i] == '\\')
+		if (t->inp[i] == '\\' && !t->heredoc)
+		{
 			i--;
-		if ((t->index - i) % 2)
-			t->bslash = 1;
+			while (i && t->inp[i] == '\\')
+				i--;
+			if ((t->bytes - i) % 2)
+				t->bslash = 0;
+			else
+				t->bslash = 1;
+		}
 		else
-			t->bslash = 0;
+			if (t->inp[i] != D_QUO && t->inp[i] != S_QUO)
+				t->bslash = 0;
 	}
-	else
-		if (t->ch != D_QUO && t->ch != S_QUO)
-			t->bslash = 0;
 }
