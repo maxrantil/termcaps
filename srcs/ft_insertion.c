9a0ecf173d41158d56f655447b1d01dc5df79580
mqx <litnar@pm.me>
2022-12-13 17:47:41 +0200

    norm

--- srcs/ft_insertion.c
+++ srcs/ft_insertion.c
@@ -46,20 +46,21 @@ static void	ft_delim_fetch(t_term *t)
 static void	ft_insertion_char(t_term *t)
 {
 	ft_putc(t->ch);
-	ft_heredoc_handling(t, t->ch, t->index);	
+	ft_heredoc_handling(t, t->ch, t->index);
 	t->c_col++;
 	ft_shift_nl_addr(t, 1);
 	if (t->inp[t->index])
 		ft_shift_insert(t);
 	t->inp[t->index++] = (char)t->ch;
 	t->bytes++;
-	if ((t->inp[t->index - 1] == D_QUO || t->inp[t->index - 1] == S_QUO) && !t->heredoc) // $> mike\"
+	if ((t->inp[t->index - 1] == D_QUO || t->inp[t->index - 1] == S_QUO) \
+		&& !t->heredoc)
 	{
 		if (!ft_bslash_escape_check(t, t->index - 1))
 			ft_quote_handling(t, t->inp[t->index - 1]);
 	}
 	if (t->inp[t->index - 1] == '\\')
-		ft_quote_flag_check(t, t->index - 1);	
+		ft_quote_flag_check(t, t->index - 1);
 }
 
 /*
@@ -72,7 +73,7 @@ static void	ft_insertion_enter(t_term *t)
 {
 	if (!t->nl_addr[t->c_row + 1])
 	{
-		t->bslash = ft_bslash_escape_check(t, t->bytes); 
+		t->bslash = ft_bslash_escape_check(t, t->bytes);
 		if (t->q_qty % 2 || (t->heredoc && (t->delim && !ft_strcmp(t->nl_addr[t->c_row], t->delim))) || t->bslash)
 		{
 			t->history_row = -1;
